8866d5138b30042dbcebc1c3cc92c9a99ecead2d
Mihail <mabekusov@edu.hse.ru>
2021-01-19 00:37:04 +0300

    Fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main() {
     std::cout << infix2postfix("(1 + 200)") << std::endl;
     std::cout << infix2postfix("(10 + 10 + 10 * 10)") << std::endl;
     std::cout << infix2postfix("550 - 362 * 152 + 250") << std::endl;
-    std::cout << infix2postfix("56465 - 564 + 62 * 15 / 65 - 56 / 51") << std::endl;
-    std::cout << infix2postfix("56465 - (564 + 62) * 15 / (65 - 56) / 51") << std::endl;
-    std::cout << infix2postfix("(52 * (15 - 25) / (36 + 43) - 85 * (250 - 89))") << std::endl;
-    std::cout << infix2postfix("52 * 15 - 25 / 36 + 43 - 85 * 250 - 89") << std::endl;
+    std::cout << infix2postfix("564 + 62 * 15 / 65 - 56 / 51") << std::endl;
+    std::cout << infix2postfix("(564 + 62) * 15 / (65 - 56) / 51") << std::endl;
+    std::cout << infix2postfix("(52 * (15 - 25) / (36 + 43))") << std::endl;
+    std::cout << infix2postfix("52 * 15 - 25 / 36 + 43 - 85 * 250") << std::endl;
     return 0;
 }
